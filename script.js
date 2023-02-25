688ef5acde1b67580a7744c892f088e66147316a
Miranda Karecho <109288708+karechob@users.noreply.github.com>
2023-02-25 12:02:38 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+let node;
+window.onload = function WindowLoad(event) {
+    node = document.getElementsByTagName('li')[0];
+    node = node.cloneNode(true);
+}
+
+function addTodoItem(event) {
+    event.preventDefault();
+    node = node.cloneNode(true);
+    node.querySelector("input").checked = false;
+
+    if(input[0].value === "") {
+        alert("You did not add anything.");
+    } else {
+        node.querySelector("#content").textContent = input[0].value;
+        if(input[1].value == "") {
+            var today = new Date();
+            var tzoffset = today.getTimezoneOffset() * 60000; //offset in milliseconds
+            var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0,10);
+            input[1].value = localISOTime;
+        }
+        node.querySelector("#date").textContent = input[1].value.replaceAll("-","/");
+        document.querySelector('ul').appendChild(node);
+    }
+}
+
+function editTask(event) {
+    let prevText = event.target.parentNode.parentNode.querySelector("#content").innerHTML;
+    console.log(prevText);
+
+    event.target.parentNode.parentNode.querySelector(".data").removeChild(event.target.parentNode.parentNode.querySelector("#content"));
+    
+    let editInputBox = document.createElement("input");
+    editInputBox.value = prevText;
+    editInputBox.addEventListener("keydown",(event) => {if(event.key == "Enter"){ replace(event); }});
+    event.target.parentNode.parentNode.append(editInputBox)
+}
+
+function replace(event) {
+    let newContent = event.target.value;
+    let newSpan = document.createElement("span");
+    newSpan.id = "content";
+    newSpan.innerHTML = newContent;
+    event.target.parentNode.querySelector(".data").append(newSpan);
+    event.target.parentNode.removeChild(event.target.parentNode.getElementsByTagName("input")[1]);
+}
+
+function checkIfChecked(event) {
+    if(event.target.checked) {
+        let checked = new Audio('checksound.mp3');
+        checked.play()
+    }
+}
